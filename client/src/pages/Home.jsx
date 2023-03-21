8c2abb2dd298aaaff7788cf4d29343012621b9d9
dukuaris <dukuaris@gmail.com>
2023-03-21 15:34:46 +0900

    Add quiz types and the number of questions, also add optional codes according the quiz types

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -30,7 +30,7 @@ const Home = ({ name, setName, unit, setUnit, fetchQuestions }) => {
 				setGeneratingQuiz(true)
 				// https://quiz-maker.onrender.com
 				// http://localhost:5001
-				const response = await fetch('http://localhost:5001', {
+				const response = await fetch('https://quiz-maker.onrender.com', {
 					method: 'POST',
 					headers: {
 						'Content-Type': 'application/json',
