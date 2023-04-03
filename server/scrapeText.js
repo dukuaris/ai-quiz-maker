9c042bee30eb5917e77a4ed83849f6d0002469dc
dukuaris <dukuaris@gmail.com>
2023-04-03 11:29:02 +0900

    Updated args and executable path for puppeteer launch

--- server/scrapeText.js
+++ server/scrapeText.js
@@ -1,8 +1,20 @@
 const puppeteer = require('puppeteer')
+require('dotenv').config()
 
 const scrapeText = async (req, res) => {
 	const url = req.body.url
-	const browser = await puppeteer.launch()
+	const browser = await puppeteer.launch({
+		args: [
+			'--disable-setuid-sandbox',
+			'--no-sandbox',
+			'--single-process',
+			'--no-zygote',
+		],
+		executablePath:
+			process.env.NODE_ENV === 'production'
+				? process.env.PUPPETEER_EXECUTABLE_PATH
+				: puppeteer.executablePath(),
+	})
 	try {
 		const page = await browser.newPage()
 		await page.goto(url)
