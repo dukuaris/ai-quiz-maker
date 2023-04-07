b021ba0e6b40a6ba18cc4ff0204138267d5001ae
dukuaris <dukuaris@gmail.com>
2023-04-07 09:43:41 +0900

    npm install pdfjs-dist and use the resource

--- client/src/utils/readPDF.js
+++ client/src/utils/readPDF.js
@@ -1,37 +1,38 @@
-import 'https://npmcdn.com/pdfjs-dist/build/pdf.js'
-
+import * as pdfjsLib from 'pdfjs-dist'
 pdfjsLib.GlobalWorkerOptions.workerSrc =
-	'https://cdn.jsdelivr.net/npm/pdfjs-dist@3.5.141/build/pdf.worker.min.js'
+	'../../node_modules/pdfjs-dist/build/pdf.worker.js'
+// import 'https://npmcdn.com/pdfjs-dist/build/pdf.js'
+// pdfjsLib.GlobalWorkerOptions.workerSrc =
+// 	'https://cdn.jsdelivr.net/npm/pdfjs-dist@3.5.141/build/pdf.worker.min.js'
 
-function Pdf2TextClass() {
-	var self = this
-	this.complete = 0
+class Pdf2TextClass {
+	constructor() {
+		this.complete = 0
+	}
 
-	this.pdfToText = function (data, callbackPageDone, callbackAllDone) {
+	pdfToText(data, callbackPageDone, callbackAllDone) {
 		console.assert(data instanceof ArrayBuffer || typeof data == 'string')
-		var loadingTask = pdfjsLib.getDocument(data)
-		loadingTask.promise.then(function (pdf) {
-			var total = pdf._pdfInfo.numPages
-			//callbackPageDone( 0, total );
-			var layers = {}
+		let loadingTask = pdfjsLib.getDocument(data)
+		loadingTask.promise.then((pdf) => {
+			let total = pdf._pdfInfo.numPages
+			let layers = {}
 			for (let i = 1; i <= total; i++) {
-				pdf.getPage(i).then(function (page) {
-					var n = page.pageNumber
-					page.getTextContent().then(function (textContent) {
-						//console.log(textContent.items[0]);0
-						if (null != textContent.items) {
-							var page_text = ''
-							var last_block = null
-							for (var k = 0; k < textContent.items.length; k++) {
-								var block = textContent.items[k]
+				pdf.getPage(i).then((page) => {
+					let n = page.pageNumber
+					page.getTextContent().then((textContent) => {
+						if (textContent.items !== null) {
+							let page_text = ''
+							let last_block = null
+							for (let k = 0; k < textContent.items.length; k++) {
+								let block = textContent.items[k]
 								if (
-									last_block != null &&
-									last_block.str[last_block.str.length - 1] != ' '
+									last_block !== null &&
+									last_block.str[last_block.str.length - 1] !== ' '
 								) {
 									if (block.x < last_block.x) page_text += '\r\n'
 									else if (
-										last_block.y != block.y &&
-										last_block.str.match(/^(\s?[a-zA-Z])$|^(.+\s[a-zA-Z])$/) ==
+										last_block.y !== block.y &&
+										last_block.str.match(/^(\s?[a-zA-Z])$|^(.+\s[a-zA-Z])$/) ===
 											null
 									)
 										page_text += ' '
@@ -39,25 +40,23 @@ function Pdf2TextClass() {
 								page_text += block.str
 								last_block = block
 							}
-
-							textContent != null && console.log('page ' + n + ' finished.') //" content: \n" + page_text);
+							textContent !== null && console.log('page ' + n + ' finished.')
 							layers[n] = page_text + '\n\n'
 						}
-						++self.complete
-						//callbackPageDone( self.complete, total );
-						if (self.complete == total) {
-							window.setTimeout(function () {
-								var full_text = ''
-								var num_pages = Object.keys(layers).length
-								for (var j = 1; j <= num_pages; j++) full_text += layers[j]
+						++this.complete
+						if (this.complete === total) {
+							window.setTimeout(() => {
+								let full_text = ''
+								let num_pages = Object.keys(layers).length
+								for (let j = 1; j <= num_pages; j++) full_text += layers[j]
 								callbackAllDone(full_text)
 							}, 1000)
 						}
-					}) // end  of page.getTextContent().then
-				}) // end of page.then
-			} // of for
+					})
+				})
+			}
 		})
-	} // end of pdfToText()
+	}
 }
 
 export default Pdf2TextClass
