6007b77edf6871f1e35ec6d62bed5a4709bee6d5
dukuaris <dukuaris@gmail.com>
2023-04-07 12:13:09 +0900

    Move getContent of PDF to Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -15,6 +15,9 @@ import ErrorMessages from '../components/ErrorMessages'
 import types from '../data/types.js'
 import '../styles/Home.css'
 import createSheet, { readSheet } from '../utils/handleSheet'
+import * as pdfjsLib from 'pdfjs-dist'
+pdfjsLib.GlobalWorkerOptions.workerSrc =
+	'../../node_modules/pdfjs-dist/build/pdf.worker.js'
 
 const Home = () => {
 	const { questions, name, unit } = useSelector((state) => state.quiz)
@@ -48,6 +51,55 @@ const Home = () => {
 			setActiveColor('')
 		}
 	}, [ready])
+
+	const getContent = async (file, callbackAllDone) => {
+		let complete = 0
+		const loadingTask = pdfjsLib.getDocument(file)
+		loadingTask.promise.then((pdf) => {
+			let total = pdf._pdfInfo.numPages
+			let layers = {}
+			for (let i = 1; i <= total; i++) {
+				pdf.getPage(i).then((page) => {
+					let n = page.pageNumber
+					page.getTextContent().then((textContent) => {
+						if (null != textContent.items) {
+							let page_text = ''
+							let last_block = null
+							for (let k = 0; k < textContent.items.length; k++) {
+								let block = textContent.items[k]
+								if (
+									last_block != null &&
+									last_block.str[last_block.str.length - 1] != ' '
+								) {
+									if (block.x < last_block.x) page_text += '\r\n'
+									else if (
+										last_block.y != block.y &&
+										last_block.str.match(/^(\s?[a-zA-Z])$|^(.+\s[a-zA-Z])$/) ==
+											null
+									)
+										page_text += ' '
+								}
+								page_text += block.str
+								last_block = block
+							}
+
+							textContent != null && console.log('page ' + n + ' finished.') //" content: \n" + page_text);
+							layers[n] = page_text + '\n\n'
+						}
+						++complete
+						if (complete == total) {
+							window.setTimeout(function () {
+								var full_text = ''
+								var num_pages = Object.keys(layers).length
+								for (var j = 1; j <= num_pages; j++) full_text += layers[j]
+								callbackAllDone(full_text)
+							}, 1000)
+						}
+					})
+				})
+			}
+		})
+	}
 
 	const handleSubmit = async () => {
 		if (ready) {
@@ -151,13 +203,13 @@ const Home = () => {
 		const file = e.target.files[0]
 		let fileReader = new FileReader()
 		fileReader.onload = () => {
-			// setCrawling(true)
-			// getContent(fileReader.result, (text) => {
-			// 	setUserInput(text)
-			// 	setForm({ ...form, content: text })
-			// 	setWordCount(text.length)
-			// 	setCrawling(false)
-			// })
+			setCrawling(true)
+			getContent(fileReader.result, (text) => {
+				setUserInput(text)
+				setForm({ ...form, content: text })
+				setWordCount(text.length)
+				setCrawling(false)
+			})
 		}
 		try {
 			fileReader.readAsDataURL(file)
